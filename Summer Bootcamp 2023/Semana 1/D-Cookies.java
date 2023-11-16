ec057d060d8f5687558e53a238e1483df4207de9
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-16 10:33:28 +0100

    Create D-Cookies.java

---
+++ Summer Bootcamp 2023/Semana 1/D-Cookies.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class cookies {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int c = scan.nextInt();
+
+        if (n % c == 0) {System.out.print("YES");}
+        else {System.out.print("NO");}
+    }
+}
