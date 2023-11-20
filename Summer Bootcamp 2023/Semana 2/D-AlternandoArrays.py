99132bb0f636a95bc0b8f9ea1739957afb8380e6
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-20 08:44:17 +0100

    Ok

---
+++ Summer Bootcamp 2023/Semana 2/D-AlternandoArrays.py
@@ -0,0 +1,18 @@
+longitud = int(input())
+
+array1 = list(map(str, input().split()))
+array2 = list(map(str, input().split()))
+
+array1_out = []
+array2_out = []
+
+for i in range(longitud):
+    if i % 2 == 0:
+        array1_out.append(array1[i])
+        array2_out.append(array2[i])
+    else:
+        array1_out.append(array2[i])
+        array2_out.append(array1[i])
+
+print(' '.join(array1_out))
+print(' '.join(array2_out))
