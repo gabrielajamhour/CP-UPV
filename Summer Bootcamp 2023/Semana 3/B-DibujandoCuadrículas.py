30d34f4328fee5bde7bbeaf51b2c21028d9aff70
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-22 10:49:57 +0100

    Delete Summer Bootcamp 2023/Semana 3/B-DibujandoCuadrículas.py

--- Summer Bootcamp 2023/Semana 3/B-DibujandoCuadrículas.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-print('+---+' + '---+' * (n-1))
-
-for _ in range(n):
-    for _ in range(3):
-        print('|   |' + '   |' * (n-1))
-        
-    print('+---+' + '---+' * (n-1))
