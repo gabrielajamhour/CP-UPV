7d2da2d2b5948c5d013310408db98fb2bc985805
Gabi <146675101+paodequeijo56@users.noreply.github.com>
2023-11-22 10:25:45 +0100

    Ok

---
+++ Summer Bootcamp 2023/Semana 3/A-Tostada.py
@@ -0,0 +1,16 @@
+lado = int(input())
+espaço1 = input()
+
+tostadaAntes = [list(map(str, input().strip())) for _ in range(lado)]
+
+espaço2 = input()
+
+tostadaDespues = [list(map(str, input().strip())) for _ in range(lado)]
+
+if all(all(elemento != '#' for elemento in linea) for linea in tostadaAntes):
+# Verifica em nenhuma das linhas de "tostadaAntes" tem mermelada
+    print("NO LLEVABA MERMELADA")
+elif tostadaAntes == tostadaDespues:
+    print("HA HABIDO SUERTE")
+else:
+    print("TRAGEDIA")
