666592b67eb9fee2e952aaea22322d8aee955d0f
Gabriela <146675101+gabrielajamhour@users.noreply.github.com>
2024-11-25 21:54:17 +0100

    Done

--- Summer Bootcamp 2023/Semana 2/C-TiendasdeHelado.py
+++ Summer Bootcamp 2023/Semana 2/C-TiendasdeHelado.py
@@ -6,4 +6,3 @@ heladeria2 = list(map(int, input().split()))
 sumaSabores = [sabor_hel1 + sabor_hel2 for sabor_hel1, sabor_hel2 in zip(heladeria1, heladeria2)]
 
 print(' '.join(map(str, sumaSabores)))
-a
